1f07e798c972c48c2f9f68b6452db995fa09ef73
prynwush <cyrusmadriaga325@gmail.com>
2026-09-22 22:53:21 +0800

    asd
    asd

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -8,16 +8,17 @@
 // 4) Deploy the included firestore.rules after replacing OWNER_EMAIL_HERE with the same owner email.
 
 export const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: "AIzaSyDPjXmx9PEFZQlKuuO8ILjyHGPM4aUe-XQ",
+  authDomain: "ph-payroll-system.firebaseapp.com",
+  projectId: "ph-payroll-system",
+  storageBucket: "ph-payroll-system.firebasestorage.app",
+  messagingSenderId: "952797118192",
+  appId: "1:952797118192:web:1050b90e102b151b8e8c35",
+  measurementId: "G-ZKMLLC6ZN8"
 };
 
 export const cloudSettings = {
   enabled: true,
-  workspaceId: 'sahod-main',
-  ownerEmail: 'madriagakenneth22@gmail.com'
+  workspaceId: "ph-payroll-main",
+  ownerEmail: "madriagakennet22@gmail.com"
 };
